b607911f77ddaa012c20121818daf612cee38482
Juliet Urbina <julieturbina@Juliets-MacBook-Pro.local>
2018-05-05 15:47:16 -0400

    game stage 1

---
+++ styles.js
@@ -0,0 +1,28 @@
+// Potential questions and answers
+console.log("Hi");
+
+//store the first name entered
+
+
+//store the "relation to Belize" botton clicked
+
+// ask the questions
+
+
+// score the questions answered correctly
+
+//total the scores
+
+//compare the scores to the rule
+
+//make a pop-up containing the player's name 
+
+var firstQuestion = {
+
+questionNumber: 1 
+questionScript: "Where is Belize located?";
+potentialAnswers: [] 
+
+}
+ 
+
