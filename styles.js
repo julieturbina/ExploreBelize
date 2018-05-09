7714aefc7f9fa82f2197c4932e0a89952b69e0ac
Juliet Urbina <julieturbina@Juliets-MacBook-Pro.local>
2018-05-09 01:07:42 -0400

    second iteration

--- styles.js
+++ styles.js
@@ -8,57 +8,62 @@ console.log("Hi!!!");
 
 // 2. store the "relation to Belize" button clicked to dictate message at the end of the game. 
 
-// ==============questions======
 
-// var relate = prompt (){
-//       var relate = "I am a Belizean",
-//       var relate = "I have been to Belize",
-//       var relate = "I have never been to Belize",
-//          var relate = "I have never heard of Belize",
-// // }
+// ==================Bank of 50 questions=========
+
+// (function() {
+//     var questions = [{
+//       question: "When did Belize become independent?",
+//       answers: [1961, 1971, 1981],
+//       correctAnswer: 2
+
+//     }, {
+//         question: "What is Juliet's nickname?",
+//         answers: ["Jules", "Julie", "Jay"],
+//         correctAnswer: 0
+//     }, {
+//       question: "What is Jules",
+//       answers: ["Girl", "Boy", "Baby"],
+//       correctAnswer: 0
+//     }, {
+//       question: "Where is Jules?",
+//       answers: ["At the mall", "At Ironhack", "At home"],
+//       correctAnswer: 1
+//     }, {
+//       question: "What is Juliet's last name?",
+//       answers: ["Urbina", "Thomas", "Sylvestre"],
+//       correctAnswer: 0
+//     }];
+// });
+    
+    // var questionCounter = 0; // question number
+    // var selections = []; //Answers
+    // var quiz = $('#game'); //Quiz game div object
+    
+                        // Display initial question
+    // displayNext();
+    
+                        // Click handler for the 'next' button
+    // $('#next').on('click', function (e) {
+                        //   e.preventDefault();
+
+// });
 
 
 // 3. questions will be 10 random questions from the bank of questions below.
-// ==================Bank of 50 questions=========
 
-// var firstQuestion = {
-
-// questionNumber: 1 
-// questionScript: "Where is Belize located?";
-// potentialAnswers: [] 
-
-// }
-
-// ==================?????==
-
-
-
-// const myQuestions = [
-//     {
-//       question: "What year did Belize become a Crown Colony?",
-//       answers: {
-//         a: "1800s",
-//         b: "1900s",
-//         c: "2000s"
-//       },
-//       correctAnswer: "a"
-//     },
-    // {
-    //   question: "What year did Belize come independence?",
-    //   answers: {
-    //     a: "1976",
-    //     b: "1981",
-    //     c: "1999",
-    //   },
-    //   correctAnswer: "b"
-    // }
-
+// function quizQuestionsIndex(){
+//     // for questionIndexArray = [
+//       for(var i = 0; i < array.length; i++){
+//         questionIndexArray.push(Math.floor(Math.random()*10));
+//       }
+//     return quizIndexArray;
+//   }
 
 // =================
 
-// 4. score the questions answered correctly automatically for Game 1 = 1 Player
 
-//5. Manually score the questions answered correctly for Game 2 = 2 Players
+//5. Manually score the questions answered correctly for 2 Players
 
 var p1Button = document.getElementById("p1");
 var p2Button = document.getElementById("p2");
@@ -108,15 +113,12 @@ resetButton.addEventListener("click", function(){
     p2Display.classList.remove("winner"); 
     gameEnd = false;
     });
-
-
-  
-// 5. total the scores for Game 1 ?/10 and compare to rule to customize message and end game after 10 questions have been issued.
+ 
+function calculateScore(){
+    alert("Explore Belize");
+}
 
 // 6.  Compare the scores for game 2 to announce the winner! No customized message will be issued as a total of 19 questions will be issued and as soon as a player scores 10, the game will end.
-
-
-// 7. Create a pop-up message  according to the game 1 player's score to the relation question answered. 
 
 // ==================
 
